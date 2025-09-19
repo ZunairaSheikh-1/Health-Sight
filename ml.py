c4b2d32e7ae5a4a675850e69109a23311bfb14a6
Zunaira Sheikh <zunairasheikh045@gmail.com>
2025-09-19 16:53:29 +0500

    ml file added

--- ml.py
+++ ml.py
@@ -1,18 +1,26 @@
 # print("Start Machine Learning")
 
-#Sum Program 
+# #Sum Program 
 
-print("Welcome to the Magical Addition Machine")
-print("Two numbers will go in... and their magical sum will come out!")
+# print("Welcome to the Magical Addition Machine")
+# print("Two numbers will go in... and their magical sum will come out!")
 
-# Asking user for input
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+# # Asking user for input
+# num1 = int(input("Enter the first number: "))
+# num2 = int(input("Enter the second number: "))
 
-# Performing sum
-sum_result = num1 + num2
+# # Performing sum
+# sum_result = num1 + num2
 
-# Fun, creative output
-print("\n Mixing the powers of", num1, "and", num2, "...")
-print("Drumroll please")
-print("The magical sum is:", sum_result,)
+# # Fun, creative output
+# print("\n Mixing the powers of", num1, "and", num2, "...")
+# print("Drumroll please")
+# print("The magical sum is:", sum_result,)
+
+
+# Squares from 1 to 10 
+
+print("Welcome to the Square Show! \n")
+
+for i in range(1, 11):
+    print(f"{i} × {i} = {i*i}")
