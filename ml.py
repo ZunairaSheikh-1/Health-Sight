35370899e362181d46ed83ee788d6115e7a520c3
Zunaira Sheikh <zunairasheikh045@gmail.com>
2025-09-19 16:47:39 +0500

    ml file added

--- ml.py
+++ ml.py
@@ -1,1 +1,18 @@
-print("Start Machine Learning")
+# print("Start Machine Learning")
+
+#Sum Program 
+
+print("Welcome to the Magical Addition Machine")
+print("Two numbers will go in... and their magical sum will come out!")
+
+# Asking user for input
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Performing sum
+sum_result = num1 + num2
+
+# Fun, creative output
+print("\n Mixing the powers of", num1, "and", num2, "...")
+print("Drumroll please")
+print("The magical sum is:", sum_result,)
