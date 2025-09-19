ec3ed98a685ccc6df34c264752f0218e0de6bd49
Zunaira Sheikh <zunairasheikh045@gmail.com>
2025-09-20 03:22:05 +0500

    ml file added

--- ml.py
+++ ml.py
@@ -46,27 +46,54 @@
 
 
 #Create a bank account app 
-class BankAccount:
-    def __init__(self, owner, balance=0):
-        self.owner = owner
-        self.balance = balance
+# class BankAccount:
+#     def __init__(self, owner, balance=0):
+#         self.owner = owner
+#         self.balance = balance
     
-    def deposit(self, amount):
-        self.balance += amount
-        print(f"Deposited {amount}, new balance = {self.balance}")
+#     def deposit(self, amount):
+#         self.balance += amount
+#         print(f"Deposited {amount}, new balance = {self.balance}")
     
-    def withdraw(self, amount):
-        if amount <= self.balance:
-            self.balance -= amount
-            print(f"Withdrawn {amount}, balance left = {self.balance}")
-        else:
-            print("Insufficient Balance!")
+#     def withdraw(self, amount):
+#         if amount <= self.balance:
+#             self.balance -= amount
+#             print(f"Withdrawn {amount}, balance left = {self.balance}")
+#         else:
+#             print("Insufficient Balance!")
 
-# Using the class
-acc1 = BankAccount("Zunaira", 500)
-acc1.deposit(200)
-acc1.withdraw(100)
+# # Using the class
+# acc1 = BankAccount("Zunaira", 500)
+# acc1.deposit(200)
+# acc1.withdraw(100)
 
 
+import time
+
+# Palindrome Checker (Creative Coding)
+
+# User input
+text = input("Enter a word or number: ")
+
+# Remove spaces + lowercase for fair comparison
+clean_text = text.replace(" ", "").lower()
+
+# Reverse the string
+reversed_text = clean_text[::-1]
+
+print("\n Checking if it's a palindrome...\n")
+time.sleep(1)
+
+# Print characters one by one (animation effect)
+for c in reversed_text:
+    print(c, end="", flush=True)
+    time.sleep(0.1)
+print("\n")
+
+# Palindrome check
+if clean_text == reversed_text:
+    print(f"Wow! '{text}' is a Palindrome")
+else:
+    print(f"'{text}' is NOT a Palindrome.")
 
 
